0549d68dc5db0ead09273e196aae3ce99c09c342
TudbuT <tudbut@tudbut.de>
2023-01-02 14:34:39 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# fediblock.directory
+
+This repo contains the files to create a table like the one seen on 
+https://fediblock.directory/#blocklist.
+
+## Usage
+
+1. Create a double-bar-speparated-values file named `blocklist_[id].dbsv` and
+   write your blocklist.
+2. Run `cargo run [id] | xclip -selection clipboard`
+3. You now have some HTML in your clipboard.
+
+## Flaws + TODO
+
+- We are currently using an async runtime for no apparent reason, but this is
+  just to be able to expand the codebase later and add more stuff.
+- We want to check if a domain still exists before displaying it to the main
+  site.
+
+## What is this for?
+
+Converting a stupid file (DBSV) to a smart file (HTML + JS).
+
+## Can I use it?
+
+Yes, but you must credit us very visibly.
---
+++ src/main.rs
@@ -0,0 +1,146 @@
+use std::{env::args, fs};
+
+use microasync::sync;
+use microasync_util::{get_current_runtime, QueuedRuntime};
+
+#[derive(Debug)]
+#[allow(unused)]
+enum BlockKind {
+    RejectMedia,
+    RejectReports,
+    Silence,
+    SilenceRejectMedia,
+    SilenceRejectReports,
+    Suspend,
+}
+
+impl From<&str> for BlockKind {
+    fn from(value: &str) -> Self {
+        if value == "RejectMedia" {
+            return BlockKind::RejectMedia;
+        }
+        if value == "RejectReports" {
+            return BlockKind::RejectReports;
+        }
+        if value == "Silence" {
+            return BlockKind::Silence;
+        }
+        if value == "SilenceRejectMedia" {
+            return BlockKind::SilenceRejectMedia;
+        }
+        if value == "SilenceRejectReports" {
+            return BlockKind::SilenceRejectReports;
+        }
+        if value == "Suspend" {
+            return BlockKind::Suspend;
+        }
+        panic!("Invalid BlockKind: {value}");
+    }
+}
+
+struct InstanceBlock {
+    url: String,
+    reason: String,
+    kind: BlockKind,
+}
+
+impl From<&str> for InstanceBlock {
+    fn from(value: &str) -> Self {
+        let mut value = value.split("||");
+        Self {
+            url: value.next().unwrap().into(),
+            reason: value.next().unwrap().into(),
+            kind: value.next().unwrap().into(),
+        }
+    }
+}
+
+fn main() {
+    println!(
+        "{}",
+        "
+<style>
+    @import url(\"https://fonts.googleapis.com/css?display=swap&amp;family=Inter:700,700italic,900,900italic%7CUbuntu:300,300italic,400,400italic\");
+
+    table {
+        width: 100%;
+    }
+    div.a {
+        display: block;
+        padding: 10px;
+        border-radius: 10px;
+        background-color: #2c2f35;
+    }
+    table, th, td {
+        border: 1px solid #789;
+        border-collapse: collapse;
+        padding: 5px;
+        color: #ddd;
+        font-family: Ubuntu sans-serif;
+        font-size: 15px;
+    }
+    td {
+        cursor: pointer;
+    }
+</style>
+
+<div class=a><table>
+    <tr>
+        <th>URL</th>
+        <th>Reason</th>
+        <th>Type</th>
+    </tr>"
+    );
+    let mut runtime = QueuedRuntime::new();
+    runtime.push(async {
+        let file = fs::read_to_string(format!(
+            "blocklist_{}.dbsv",
+            args().skip(1).next().expect("please provide the category")
+        ))
+        .expect("file not found: blocklist_{arg}.dbsv!");
+
+        for line in file.replace("\r\n", "\n").split("\n") {
+            if line.is_empty() {
+                continue;
+            }
+            get_current_runtime()
+                .await
+                .push(add(InstanceBlock::from(line)));
+        }
+    });
+    sync(runtime);
+    println!("</table></div>");
+}
+
+async fn add(block: InstanceBlock) {
+    // maybe add dns check here later
+    let (url, reason, kind) = (
+        block
+            .url
+            .replace("<", "&lt;")
+            .replace(">", "&gt;")
+            .replace("\"", "&quot;"),
+        block
+            .reason
+            .replace("<", "&lt;")
+            .replace(">", "&gt;")
+            .replace("\"", "&quot;"),
+        format!("{:?}", block.kind)
+            .replace("<", "&lt;")
+            .replace(">", "&gt;")
+            .replace("\"", "&quot;"),
+    );
+    println!(
+        "    <tr>
+        <td onclick=\"navigator.clipboard.writeText('{}');\">{}</td>
+        <td onclick=\"navigator.clipboard.writeText('{}');\">{}</td>
+        <td onclick=\"navigator.clipboard.writeText('{}');\">{}</td>
+    </tr>",
+        url.replace("\\", "\\\\").replace("\'", "\\\'"),
+        url,
+        reason.replace("\\", "\\\\").replace("\'", "\\\'"),
+        reason,
+        kind.replace("\\", "\\\\").replace("\'", "\\\'"),
+        kind,
+    );
+}
